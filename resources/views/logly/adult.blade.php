9c2d2ccd90846914b779d605d6b1e0a733a2e6e3
EC2 Default User <ec2-user@ip-172-31-4-142.us-east-2.compute.internal>
2020-10-03 19:44:12 +0000

    Mm

---
+++ resources/views/logly/adult.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>女子●生が暴露！？今話題のアプリとは…</title>
+        <link href="{{ secure_asset('css/adult.css') }}" rel="stylesheet">
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    </head>
+    <body>
+        <img src="{{ asset('image/IMG_4770.jpg') }}" class="protect"></img>
+        <img src="{{ asset('image/IMG_4771.jpg') }}" class="protect"></img>
+        <img src="{{ asset('image/IMG_4772.jpg') }}" class="protect"></img>
+        <img src="{{ asset('image/IMG_4773.jpg') }}" class="protect"></img>
+        <img src="{{ asset('image/IMG_4774.jpg') }}" class="protect"></img>
+        <img src="{{ asset('image/IMG_4775.jpg') }}" class="protect"></img>
+        <img src="{{ asset('image/IMG_4776.jpg') }}" class="protect"></img>
+        <img src="{{ asset('image/IMG_4777.jpg') }}" class="protect"></img>
+        <img src="{{ asset('image/IMG_4778.jpg') }}" class="protect"></img>
+        <a href="https://a-trade.jp/redirect/match?media=G16531" target="_blank"><img src="{{ asset('image/IMG_4779.jpg') }}"></img></a>
+        <img src="{{ asset('image/IMG_4780.JPG') }}" class="protect"></img>
+        <img src="{{ asset('image/IMG_4781.JPG') }}" class="protect"></img>
+        <img src="{{ asset('image/IMG_4782.JPG') }}" class="protect"></img>
+        <img src="{{ asset('image/IMG_4783.jpg') }}" class="protect"></img>
+        <img src="{{ asset('image/IMG_4784.jpg') }}" class="protect"></img>
+        <img src="{{ asset('image/IMG_4785.jpg') }}" class="protect"></img>
+        <img src="{{ asset('image/IMG_4786.jpg') }}" class="protect"></img>
+        <img src="{{ asset('image/IMG_4787.jpg') }}" class="protect"></img>
+        <a href="https://a-trade.jp/redirect/match?media=G16531" target="_blank"><img src="{{ asset('image/IMG_4788.jpg') }}"></img></a>
+        <img src="{{ asset('image/IMG_4789.jpg') }}" class="protect"></img>
+        <img src="{{ asset('image/IMG_4790.JPG') }}" class="protect"></img>
+        <img src="{{ asset('image/IMG_4791.JPG') }}" class="protect"></img>
+        <img src="{{ asset('image/IMG_4792.JPG') }}" class="protect"></img>
+        <img src="{{ asset('image/IMG_4793.jpg') }}" class="protect"></img>
+        <img src="{{ asset('image/IMG_4794.jpg') }}" class="protect"></img>
+        <a href="https://a-trade.jp/redirect/match?media=G16531" target="_blank"><img src="{{ asset('image/kokoko-2.jpg') }}"></img></a>
+        <img src="{{ asset('image/IMG_4796.jpg') }}" class="protect"></img>
+        <img src="{{ asset('image/IMG_4797.jpg') }}" class="protect"></img>
+    </body>
+</html>
