bac9364419d1d0874743d3612db806644ecbd916
EC2 Default User <ec2-user@ip-172-31-4-142.us-east-2.compute.internal>
2020-10-13 11:53:42 +0000

    mm

--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,8 @@ Route::get('cccheck2', function () {
 // ドコモアド
 // 獲得用
 Route::get('matching', function () {
-    return view('logly.omiai');
-    // return redirect('https://t.afi-b.com/visit.php?guid=ON&a=R100461-H3470814&p=37183994');
+    // return view('logly.omiai');
+    return redirect('https://t.afi-b.com/visit.php?guid=ON&a=R100461-H3470814&p=37183994');
     
     // return view('logly.noMatch'); //アダルト訴求 
     // return view('logly.matching'); //Tapple_LP参考
@@ -38,8 +38,8 @@ Route::get('matching', function () {
 // LOGLY
 // 獲得用
 Route::get('dating', function () {
-    return view('logly.omiai');
-    // return redirect('https://preaf.jp/pa.do?s=v04031&o=45765&guid=ON'); //トークウィズ - Preaf
+    // return view('logly.omiai');
+    return redirect('https://preaf.jp/pa.do?s=v04031&o=45765&guid=ON'); //トークウィズ - Preaf
 });
 
 // Omiai_DL
