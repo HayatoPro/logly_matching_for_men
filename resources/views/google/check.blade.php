f6b3b7dc8774a1aed9d62e124d0bf333dc416865
EC2 Default User <ec2-user@ip-172-31-4-142.us-east-2.compute.internal>
2020-12-17 00:12:33 +0000

    Mm

--- resources/views/google/check.blade.php
+++ resources/views/google/check.blade.php
@@ -10,7 +10,10 @@
     </head>
     <body>
         <br>
-        <img src="{{ asset('image/top.jpg') }}"></img>
-        <p>公式ストアへ遷移します。再度<a href="https://apps.apple.com/jp/app/17live-%E3%82%A4%E3%83%81%E3%83%8A%E3%83%8A-%E3%83%A9%E3%82%A4%E3%83%96%E9%85%8D%E4%BF%A1-%E3%82%A2%E3%83%97%E3%83%AA/id988259048">コチラ</a>をタップして下さい。</p>
+        <img src="{{ asset('image/livelivelive.png') }}"></img>
+        <p>公式ストアへ遷移します。再度<a href="https://apps.apple.com/jp/app/pococha-%E3%83%9D%E3%82%B3%E3%83%81%E3%83%A3-%E3%83%A9%E3%82%A4%E3%83%96%E9%85%8D%E4%BF%A1-%E3%82%A2%E3%83%97%E3%83%AA/id1175969205">コチラ</a>をタップして下さい。</p>
+        <br>
+            <br>
+            <br>
     </body>
 </html>
