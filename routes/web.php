486b5beca50761df2792b880f2be5921ce22bb3c
EC2 Default User <ec2-user@ip-172-31-4-142.us-east-2.compute.internal>
2020-12-17 11:50:01 +0000

    mm

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,8 @@
 */
 // 男性向けアダルトライブ獲得用
 Route::get('live', function () {
-    return view('google.check'); //仮LP
-    // return view('logly.noMatch'); //アダルト訴求
+    // return view('google.check'); //仮LP
+    return view('logly.noMatch'); //アダルト訴求
 });
 
 //OS判定
