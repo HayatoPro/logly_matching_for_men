52d1aca7dd667652ac865594d09213678bd0129e
EC2 Default User <ec2-user@ip-172-31-4-142.us-east-2.compute.internal>
2021-03-11 08:53:20 +0000

    m

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -10,10 +10,10 @@ class AdController extends Controller
     public function os(Request $request) {
         $user_agent =  $request->header('User-Agent');
         if ((strpos($user_agent, 'iPhone') !== false)) {
-            return redirect('https://preaf.jp/pa.do?s=v04031&o=49178&guid=ON'); //paddy67 - Preaf
+            return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=18721'); //グッディ - fam
             // return redirect('https://a-trade.jp/redirect/fatey-multi?media=G16531'); //FATEY - Aトレード
         }
-        return redirect('https://preaf.jp/pa.do?s=v04031&o=49178&guid=ON'); //paddy67 - Preaf
+        return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=18721'); //グッディ - fam
         // return redirect('https://a-trade.jp/redirect/fatey-multi?media=G16531'); //FATEY - Aトレード
     }
 }
