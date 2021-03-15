c5cc3bcde0777db94bbc3532e29aea5819db9785
EC2 Default User <ec2-user@ip-172-31-4-142.us-east-2.compute.internal>
2021-03-15 12:05:25 +0000

    m

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,8 @@
 */
 // 男性向けアダルトライブ獲得用
 Route::get('new1', function () {
-    return view('google.check'); //仮LP
-    // return view('logly.noMatch'); //アダルト訴求
+    // return view('google.check'); //仮LP
+    return view('logly.noMatch'); //アダルト訴求
     
     // return redirect('https://a-trade.jp/redirect/tokyolivetalk?media=G16531'); //VideoLiveTalk - Aトレード
 });
